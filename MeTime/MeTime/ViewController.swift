99304cd09a7a7c0e2770fc48c2bfbd745266ba6a
iuraiura879 <iuraiura879@gmail.com>
2016-10-20 22:45:38 +0300

    Initial

---
+++ MeTime/MeTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeTime
+//
+//  Created by Iura Gutu on 20/10/2016.
+//  Copyright © 2016 Iurie Gutu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
